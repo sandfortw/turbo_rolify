18d4f783b4401a055d01da26549868e52857fcd1
Weston Sandfort <sandfortw@gmail.com>
2024-04-19 10:35:40 -0600

    Create set_current_user method for appctrlr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user ||= User.find(session[:user_id])
+  end
 
 end
