290c67527f46fa264c918358d5aac671c2d08792
Weston Sandfort <sandfortw@gmail.com>
2024-04-19 11:05:20 -0600

    Add basic auth

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,2 +1,11 @@
 class AdminDashboardController < ApplicationController
+
+  def index
+    if @current_user && @current_user.roles.where(name: 'admin') != []
+      "Happy Path"
+    else
+      flash[:error] = "Unauthorized"
+      redirect_to root_path
+    end
+  end
 end
