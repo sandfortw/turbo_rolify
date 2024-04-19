ab2417a7b2db83422c327448873863249018abb7
Weston Sandfort <sandfortw@gmail.com>
2024-04-18 22:17:41 -0600

    Add view content for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   validates_presence_of :password, require: true
   has_secure_password
   rolify
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:user) if self.roles.blank?
+  end
+
+  def role 
+    self.roles.first.name
+  end
+
 end
